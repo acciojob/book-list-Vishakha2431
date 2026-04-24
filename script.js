410851c3a36216defd316cac203a068a94ec284f
Vishakha Gautam <156678713+Vishakha2431@users.noreply.github.com>
2026-04-24 23:00:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+
+let btn = document.getElementById("submit");
+
+btn.onclick = function() {
+
+  let t = document.getElementById("title").value;
+  let a = document.getElementById("author").value;
+  let i = document.getElementById("isbn").value;
+
+  let tbody = document.getElementById("book-list");
+
+  tbody.innerHTML += 
+  "<tr>" +
+  "<td>" + t + "</td>" +
+  "<td>" + a + "</td>" +
+  "<td>" + i + "</td>" +
+  "<td><button onclick='deleteRow(this)'>Clear</button></td>" +
+  "</tr>";
+}
+
+
+function deleteRow(btn) {
+  btn.parentElement.parentElement.remove();
+}
